0e9f1817eb96222a6e2d9f32080996502a7714f3
Ahmed Mourad <AMourad@healthinsightsgroup.com>
2025-09-17 01:00:09 +0300

    add title

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+
   return (
     <Html lang="en">
+    <title>الهاكاثون</title>
       <Head />
       <body className="antialiased">
         <Main />
